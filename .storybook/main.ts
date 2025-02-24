fe88b8e01ddfb1f7ffab083145bef8bcbc1369cd
Enzo M <enzojmayo@gmail.com>
2025-02-23 18:54:43 -0600

    Remove unneeded packages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,11 +2,18 @@ import type { StorybookConfig } from "@storybook/web-components-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-essentials", "@chromatic-com/storybook"],
+
+  addons: [
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-a11y"
+  ],
+
   framework: {
     name: "@storybook/web-components-vite",
     options: {},
   },
-  
+
+  docs: {}
 };
 export default config;
--- test/griddle-grid.test.ts
+++
@@ -1,8 +0,0 @@
-import {expect} from '@open-wc/testing'
-import '../src/index'
-
-it('Grid custom element exists', () => {
-    const gridElement = customElements.get('gdl-data-grid');
-    expect(gridElement).to.not.be.undefined;
-});
-
