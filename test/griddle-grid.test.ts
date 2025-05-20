07b08a203ddd79b4f713cbe399423d797caa3363
Enzo M <enzojmayo@gmail.com>
2025-05-20 17:59:56 -0500

    Add test suites

--- test/griddle-grid.test.ts
+++ test/griddle-grid.test.ts
@@ -1,8 +1,9 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { render } from 'vitest-browser-lit'
 import { html } from 'lit'
 import { page, userEvent } from '@vitest/browser/context';
 import '../src/index';
+import { GriddleDataGrid } from '../src/index';
 
 const BASE_TEMPLATE = html`
     <button data-testid="1">Anchor to focus on</button>
@@ -22,44 +23,55 @@ const BASE_TEMPLATE = html`
         </gdl-grid-row>
     </gdl-data-grid>
     `;
-    
 
-const preparePageForTabbing = async (template=BASE_TEMPLATE) => {
+
+const preparePageForTabbing = async (template = BASE_TEMPLATE) => {
     const screen = page.render(template);
     const anchor = screen.getByTestId('1');
     await anchor.click();
     const gridLocator = screen.getByRole('grid');
-    
-    return {gridLocator, screen};
+
+    return { gridLocator, screen };
 };
 
-test('renders grid with text cells', async () => {
-    const screen = render(BASE_TEMPLATE);
+describe('Custom Elements have correct fields and exist', () => {
+    test('renders grid and grid is visible', async () => {
+        const screen = render(BASE_TEMPLATE);
 
-    const element = screen.getByRole('grid');
-    await expect.element(element).toBeInTheDocument();
+        const element = screen.getByRole('grid');
+        await expect.element(element).toBeInTheDocument();
+        await expect.element(element).toBeInstanceOf(GriddleDataGrid);
+        await expect.element(element).toBeVisible();
+
+    });
+})
+
+describe('Keyboard Navigation', () => {
+
+    describe('Tab Navigaton - Initial Tab Order', () => {
+        //
+        test('Tabs to grid from body', async () => {
+            const { gridLocator } = await preparePageForTabbing();
+            // Sets us up for actually testing the tab order.
+            // Tab from the body element to the data grid.
+            await userEvent.tab();
+            await expect.element(gridLocator).toHaveFocus();
+
+        });
+
+        test('Tabs to first cell of the grid from focus on the grid', async () => {
+            const { gridLocator, screen } = await preparePageForTabbing();
+            // Sets us up for actually testing the tab order.
+            const firstCell = screen.getByRole('gridcell').first();
+            // Tab from the body element to the data grid.
+            await userEvent.tab();
+            await expect.element(gridLocator).toHaveFocus();
+
+            // Tab to the first available cell of the grid.
+            await userEvent.tab();
+            await expect.element(firstCell, { timeout: 1000 }).toHaveFocus();
+        });
+    });
 
     
-});
-
-test('Tabs to grid from body', async () => {
-    const {gridLocator} = await preparePageForTabbing();
-    // Sets us up for actually testing the tab order.
-    // Tab from the body element to the data grid.
-    await userEvent.tab();
-    await expect.element(gridLocator).toHaveFocus();
-    
-});
-
-test('Tabs to first cell of the grid from focus on the grid', async () => {
-    const {gridLocator, screen} = await preparePageForTabbing();
-    // Sets us up for actually testing the tab order.
-    const firstCell = screen.getByRole('gridcell').first();
-    // Tab from the body element to the data grid.
-    await userEvent.tab();
-    await expect.element(gridLocator).toHaveFocus();
-
-    // Tab to the first available cell of the grid.
-    await userEvent.tab();
-    await expect.element(firstCell, {timeout: 1000}).toHaveFocus();
-});
+})
